20394cb5cc2ef8acbcb67baeea6f90c231919d67
helenecourau <helene.courau@gmail.com>
2020-05-08 17:22:45 +0200

    add constants

---
+++ website/constants.py
@@ -0,0 +1,4 @@
+#URL
+general = "https://fr.openfoodfacts.org/cgi/"
+research = "search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0="
+format_research = "&page_size=500&json=1"
